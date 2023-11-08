cb93f3688bb83d1b946541a0e0ffeff49b6ba061
Mashtixx <nquanghuy3456@gmail.com>
2023-11-08 18:27:31 -0500

    Add more enemies

---
+++ Viscous-Vengeance-project/Assets/Scripts/WizardController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardController : EnemyController
+{
+    [SerializeField] bool ShootFireBall;
+    [SerializeField] public float fireBallInterval = 2.0f;
+
+    public GameObject Fireball;
+
+
+     void Start()
+    {
+        // Start shooting fireballs when the WizardController is enabled
+        StartCoroutine(ShootFireballs());
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    IEnumerator ShootFireballs()
+    {
+        while (ShootFireBall == true)
+        {
+            Speed = 0;
+            animator.SetBool("enemyAttack", true);
+            Instantiate(Fireball, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(fireBallInterval);
+        }
+    }
+}
